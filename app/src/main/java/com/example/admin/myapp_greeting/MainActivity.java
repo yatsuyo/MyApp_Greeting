c3ff03714b7585ccb4899b22e95adc307aca304c
yatsuyo <hungry_soul_0122@yahoo.co.jp>
2016-07-04 21:56:39 +0900

    Lesson4_Greeting

--- app/src/main/java/com/example/admin/myapp_greeting/MainActivity.java
+++ app/src/main/java/com/example/admin/myapp_greeting/MainActivity.java
@@ -1,3 +1,4 @@
+// 全て処理を扱う
 package com.example.admin.myapp_greeting;
 
 import android.app.TimePickerDialog;
@@ -8,83 +9,71 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
+
+import android.app.Activity;
+import java.util.Calendar;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView mTextView;
     EditText mEditText;
 
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // TimePickerDialog表示＆設定ボタン
         Button button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(this);
 
+
+        // 現在時刻を取得
+        Calendar calendar = Calendar.getInstance();
+        int hour   = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+
+        // 時間選択ダイアログの生成
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        // 設定 ボタンクリック時の処理
+                    }
+                },
+                hour,
+                minute,
+                true);
+
+        // あいさつ表示ボタン
         Button button2 = (Button) findViewById(R.id.button2);
         button2.setOnClickListener(this);
 
-        Button button3 = (Button) findViewById(R.id.button3);
-        button3.setOnClickListener(this);
-
-        mTextView = (TextView) findViewById(R.id.textView);
-        mEditText = (EditText) findViewById(R.id.editText);
+        // あいさつ表示欄
+        mTextView.setText(hour + ":" + minute);
+        //mTextView = (TextView) findViewById(R.id.textView);
     }
 
+    // ボタンなどを押したときの処理
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.button1) {
-            mTextView.setText(mEditText.getText().toString());
+            TimePickerDialog();
+            //hour = timePicker.getHour();
+            //minute = TimePickerDialog.hourOfDay();
         } else if (v.getId() == R.id.button2) {
-            showAlertDialog();
-        } else if (v.getId() == R.id.button3) {
-            showTimePickerDialog();
+            //mTextView.setText(hour + ":" + minute);
+            mTextView.setText(mEditText.getText().toString());
         }
     }
 
-    private void showAlertDialog() {
-        // AlertDialog.Builderクラスを使ってAlertDialogの準備をする
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-        alertDialogBuilder.setTitle("タイトル");
-        alertDialogBuilder.setMessage("メッセージ");
-
-        // 肯定ボタンに表示される文字列、押したときのリスナーを設定する
-        alertDialogBuilder.setPositiveButton("肯定",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Log.d("UI_PARTS", "肯定ボタン");
-                    }
-                });
-
-        // 中立ボタンに表示される文字列、押したときのリスナーを設定する
-        alertDialogBuilder.setNeutralButton("中立",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Log.d("UI_PARTS", "中立ボタン");
-                    }
-                });
-
-        // 否定ボタンに表示される文字列、押したときのリスナーを設定する
-        alertDialogBuilder.setNegativeButton("否定",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Log.d("UI_PARTS", "否定ボタン");
-                    }
-                });
-
-        // AlertDialogを作成して表示する
-        AlertDialog alertDialog = alertDialogBuilder.create();
-        alertDialog.show();
-    }
-
-    private void showTimePickerDialog() {
+    private void TimePickerDialog() {
         TimePickerDialog timePickerDialog = new TimePickerDialog(this,
                 new TimePickerDialog.OnTimeSetListener() {
                     @Override
@@ -93,8 +82,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     }
                 },
                 13, // 初期値（時間）
-                0,  // 初期値（分）
+                6,  // 初期値（分）
                 true);
         timePickerDialog.show();
+
     }
+
+
+
 }
